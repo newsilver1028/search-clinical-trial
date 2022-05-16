15469031479d0a9bc43f2529a7d3e53e83e228a7
GAEUN SHIN <newsilver1028@naver.com>
2022-05-16 12:00:25 +0900

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,24 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { I18nextProvider } from 'react-i18next'
-import { RecoilRoot } from 'recoil'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-
-import { store } from './states'
-import i18n from 'utils/locale'
-import reportWebVitals from './reportWebVitals'
-import './styles/index.scss'
-
-import Routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
+import './styles/index.scss';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnMount: false } },
-})
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools />
-        <Provider store={store}>
-          <RecoilRoot>
-            <BrowserRouter>
-              <Routes />
-            </BrowserRouter>
-          </RecoilRoot>
-        </Provider>
-      </QueryClientProvider>
-    </I18nextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+);
