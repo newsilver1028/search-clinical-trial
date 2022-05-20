5b48972ae2d8bd9a047c0de19d76617fb2021129
GAEUN SHIN <newsilver1028@naver.com>
2022-05-20 23:53:06 +0900

    fix: remove axios cancelTocken

--- src/hook/fetchSearchList.ts
+++ src/hook/fetchSearchList.ts
@@ -4,28 +4,17 @@ import { uniqBy } from 'lodash';
 const serviceKey = 'HDqjNstfdCbTtqxIYVEIlanJjeOBLkalHbPIFPBc1S1ze298Lu0DZhdGWRO1DkMDGOoPLbuSD1oB51OBh5YWcQ%3D%3D';
 
 const fetchSearchList = (params: string) => {
-  const request = axios.CancelToken.source();
   return axios
     .get(
-      `/B551182/diseaseInfoService/getDissNameCodeList?pageNo=1&numOfRows=50&ServiceKey=${serviceKey}&searchText=${params}&_type=json`,
-      {
-        cancelToken: request.token,
-      }
+      `/B551182/diseaseInfoService/getDissNameCodeList?pageNo=1&numOfRows=50&ServiceKey=${serviceKey}&searchText=${params}&_type=json`
     )
     .then((res) => {
       const {
-        // totalCount,
         items: { item },
       } = res.data.response.body;
-      // if (totalCount === 1) return [item];
       const removedDuplicate = uniqBy(item, (r: any) => r.sickCd);
       const removedDuplicateArray = Array.from(removedDuplicate);
       return removedDuplicateArray;
-    })
-    .catch(function (thrown) {
-      if (axios.isCancel(thrown)) {
-        console.log('request canceled');
-      }
     });
 };
 export default fetchSearchList;
